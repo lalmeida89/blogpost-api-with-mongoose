19989d9089228b553d83584e70fa5d88a7dd44cb
lalmeida89 <lalmeida89@gmail.com>
2019-02-06 23:29:19 -0500

    testing testing 12 12

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 const express = require("express");
 const morgan = require("morgan");
-
 const bprouter = require("./blogPostsRouter");
 
 const app = express();
-
-app.use(morgan("common"));
 app.use(express.json());
-
-// you need to import `blogPostsRouter` router and route
-
+app.use(morgan("common"));
 // requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
 app.use('/blog-posts', bprouter);
+// log the http layer
+app.use(morgan("common"));
+app.use(express.static("public"));
+app.use('/blog-posts', bprouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
---
+++ test/test-server.js
@@ -0,0 +1,59 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { app, runServer, closeServer } = require('../server');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+function lorem() {
+  return (
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
+    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+    "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+    "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
+    "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non " +
+    "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  );
+}
+
+describe('Blog Posts', function(){
+  before(function(){
+    return runServer()
+  })
+  after(function(){
+    return closeServer()
+  })
+  it('should list blog posts on GET', function(){
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res){
+        expect(res).to.have.status(200)
+        expect(res).to.be.json
+        expect(res.body).to.be.an('array')
+        expect(res.body.length).to.be.at.least(1)
+        const expectedKeys = ['id', 'title', 'content', 'author']
+        res.body.forEach(function(post){
+          expect(post).to.be.an("object")
+          expect(post).to.include.keys(expectedKeys)
+        })
+      })
+  })
+  it('should add new blog post on POST', function(){
+    const newPost = {
+      title:'blog title test',
+      content: lorem(),
+      author: 'bob burnquist'
+    }
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(newPost)
+      .then(function(res){
+        console.log(res.body);
+        newPost['id'] = res.body.id
+        newPost['publishDate'] = res.body.publishDate
+        expect(res.body).to.deep.equal(newPost)
+        expect(res).to.have.status(201)
+      })
+  })
+})
