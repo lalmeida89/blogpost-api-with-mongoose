346f763fd292f917f802e92a50694b4c7777ef0e
lalmeida89 <lalmeida89@gmail.com>
2019-02-12 17:42:01 -0500

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://admin:admin1@ds219181.mlab.com:19181/blogpost-with-comments";
+  process.env.DATABASE_URL || "mongodb://localhost/blogpost-with-comments";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-blogposts";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-blogpost-with-comments";
 exports.PORT = process.env.PORT || 8080;
